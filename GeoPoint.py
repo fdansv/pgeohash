705500a633e058b04991e11a2d53ec00f66b71eb
Francisco Dans <fdansv@gmail.com>
2013-09-18 17:27:29 +0100

    starting to add bounding box, very unpythonian

--- GeoPoint.py
+++ GeoPoint.py
@@ -1,8 +1,4 @@
 class GeoPoint:
-    pointLat = 0.0
-    pointLon = 0.0
-    theHash = ""
-    precision = 0
 
     def __init__(self, lat, lon, precision):
         if lat > 90 or lat < -90:
@@ -44,3 +40,31 @@ class GeoPoint:
 
 point = GeoPoint(51.52153, -5.1274, 10)
 print point.theHash
+
+
+class BoundingBox:
+    coords = []
+    n = 90
+    s = -90
+    e = -180
+    w = 180
+    geoPoint = GeoPoint()
+    precision = 0
+
+    def __init__(self, hash):
+        self.hash = hash
+        self.fromHash()
+        self.calculatePrecision()
+
+    def fromHash(self):
+        hashCopy = self.hash
+        while hashCopy != "":
+            if hashCopy[0:2] == "0":
+                self.e = (self.e + self.w)/2
+            else:
+                self.w = (self.e + self.w)/2
+            if hashCopy[1:3] == "0":
+                self.s = (self.n + self.s)/2
+            else:
+                self.n = (self.n + self.s)/2
+            hashCopy = hashCopy[2:]
